661c9da3f0a88ae291b44dbc312883efc49a930a
Gabriel <oliveirafrerreira97@hotmail.com>
2025-12-27 15:05:49 -0300

    feat(controller): criação do controller com os dois metodos responsaveis pelo funcionamento da aplicação junto com os dtos necessarios

--- src/main/java/com/gabrielbkx/notasSecretas/controller/NotaController.java
+++ src/main/java/com/gabrielbkx/notasSecretas/controller/NotaController.java
@@ -1,7 +1,39 @@
 package com.gabrielbkx.notasSecretas.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.gabrielbkx.notasSecretas.model.Nota;
+import com.gabrielbkx.notasSecretas.model.dto.DadosBuscarNotas;
+import com.gabrielbkx.notasSecretas.model.dto.DadosSalvarNotas;
+import com.gabrielbkx.notasSecretas.service.NotaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@RequestMapping("api/notas")
 public class NotaController {
+
+    private final NotaService service;
+
+    public NotaController(NotaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<DadosBuscarNotas> salvarNota(@RequestBody DadosSalvarNotas dadosNota) {
+        Nota novaNota = service.criarNota(dadosNota.conteudo());
+
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{token}")
+                .buildAndExpand(novaNota.getToken())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(new DadosBuscarNotas(novaNota.getToken()));
+    }
+
+    @GetMapping("/{token}")
+    public ResponseEntity<DadosSalvarNotas> lerNota(@PathVariable String token) {
+        var nota = service.lerNota(token);
+        return ResponseEntity.ok(new DadosSalvarNotas(nota));
+    }
+
 }
